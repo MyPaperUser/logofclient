932009efe9dc24e9dec8da1eb7c9583215d5581c
Elias Fierke <elias.fierke@outlook.com>
2025-07-01 21:59:14 +0200

    [chore:] simple test-case for address-check

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,3 +1,4 @@
+using System;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 
@@ -8,30 +9,48 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        try
+        {
+            var temppath = "kaspersons.csv";
+            var result = DataImport.ImportKasAddressList(temppath);
+            if (result.Item1)
+            {
+                var check_result = AddressCheck.Perform(result.Item2);
+                foreach (var item in check_result)
+                {
+                    Console.WriteLine();
+                    Console.Write(item.Item1 + " ");
+                    foreach (var error in item.Item2) Console.Write(error + ", ");
+                }
+            }
+        }
+        catch
+        {
+        }
     }
 
     private void MnuExit_OnClick(object? sender, RoutedEventArgs e)
     {
-        throw new System.NotImplementedException();
+        throw new NotImplementedException();
     }
 
     private void MnuAbout_OnClick(object? sender, RoutedEventArgs e)
     {
-        throw new System.NotImplementedException();
+        throw new NotImplementedException();
     }
 
     private void MnuSettings_OnClick(object? sender, RoutedEventArgs e)
     {
-        throw new System.NotImplementedException();
+        throw new NotImplementedException();
     }
 
     private void MnuHelp_OnClick(object? sender, RoutedEventArgs e)
     {
-        throw new System.NotImplementedException();
+        throw new NotImplementedException();
     }
 
     private void MnuGithub_OnClick(object? sender, RoutedEventArgs e)
     {
-        throw new System.NotImplementedException();
+        throw new NotImplementedException();
     }
 }
