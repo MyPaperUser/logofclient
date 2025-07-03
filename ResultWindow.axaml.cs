d560346365cb6ee20d0475065139817e3d69fd85
Elias Fierke <elias@mypapertown.de>
2025-07-03 11:06:54 +0200

    [chore:] added initial ResultWindow

---
+++ ResultWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Logof_Client;
+
+public partial class ResultWindow : Window
+{
+    public ResultWindow()
+    {
+        InitializeComponent();
+    }
+}
